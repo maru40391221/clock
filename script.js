121a4bc2c3d384f62b6d156a6cc096bebb8128a0
maru40391221 <polorll.1221@gmail.com>
2023-04-18 18:04:31 +0900

    now it is constantly updated
    make it look fancy with CSS tomorrow :)

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-const clockBody = document.querySelector(".clockBody");
-const year = document.querySelector("#year");
-const month = document.querySelector("#month");
-const day = document.querySelector("#day");
-const hour = document.querySelector("#hour");
-const minute = document.querySelector("#minute");
-const second = document.querySelector("#second");
+
+setInterval(update, 100);
+
+function update (){
+
+console.log("update");
 
 const date = new Date();
 
-
-
-startTime = setInterval(update, 10);
-
-function update (){
 document.getElementById("year").innerHTML = date.getFullYear();
 document.getElementById("month").innerHTML = date.getMonth() + 1;
 document.getElementById("day").innerHTML = date.getDate();
@@ -21,12 +15,3 @@ document.getElementById("minute").innerHTML = date.getMinutes();
 document.getElementById("second").innerHTML = date.getSeconds();
 
 }
-
-/*
-document.getElementById("year").innerHTML = date.getFullYear();
-document.getElementById("month").innerHTML = date.getMonth() + 1;
-document.getElementById("day").innerHTML = date.getDate();
-document.getElementById("hour").innerHTML = date.getHours();
-document.getElementById("minute").innerHTML = date.getMinutes();
-document.getElementById("second").innerHTML = date.getSeconds();
-*/
